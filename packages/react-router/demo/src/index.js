64f143dada2fd71a590513fbaadd20c95b412478
Nelson Reina (@nelreina) <nelreina@gmail.com>
2018-06-09 17:48:15 -0400

    react-router: utils for react router with private route

---
+++ packages/react-router/demo/src/index.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+import { PrivateRoute, Login } from "../../src";
+
+const PublicRoute = () => <h3>This Route is public</h3>;
+const RestrictedRoute = () => <h3>This Route is restricted!</h3>;
+
+class Demo extends Component {
+  state = {
+    auth: {
+      isAuthenticated: false
+    }
+  };
+  login = () => {
+    this.setState(() => ({ auth: { isAuthenticated: true } }));
+  };
+  logout = () => {
+    this.setState(() => ({ auth: { isAuthenticated: false } }));
+  };
+  render() {
+    const { auth } = this.state;
+    return (
+      <Router>
+        <div>
+          <h1>nr-react-private-route Demo</h1>
+          <ul>
+            <li>
+              <Link to="/public">Public</Link>
+            </li>
+            <li>
+              <Link to="/private">Private</Link>
+            </li>
+            {auth.isAuthenticated && (
+              <li>
+                <button onClick={this.logout}>Logout</button>
+              </li>
+            )}
+          </ul>
+          <pre>{JSON.stringify(auth, null, 2)}</pre>
+          <Switch>
+            <Route
+              path="/login"
+              render={props => (
+                <Login auth={auth} {...props}>
+                  <div>
+                    <h3>Please login!</h3>
+                    <button onClick={this.login}>Login</button>
+                  </div>
+                </Login>
+              )}
+            />
+            <Route path="/public" component={PublicRoute} />
+            <PrivateRoute
+              auth={auth}
+              path="/private"
+              component={RestrictedRoute}
+            />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+render(<Demo />, document.querySelector("#demo"));
