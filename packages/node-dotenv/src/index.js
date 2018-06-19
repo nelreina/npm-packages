5158a632387128deec960f0f4bfc46996d468169
Nelson Reina (@nelreina) <nelreina@gmail.com>
2018-06-19 09:43:54 -0400

    @nelreina/dotenv: just return the text iso

--- packages/node-dotenv/src/index.js
+++ packages/node-dotenv/src/index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const path = require('path');
+// const path = require('path');
 const { promisify } = require('util');
 const { each, assign } = require('lodash');
 const { EOL } = require('os');
@@ -7,12 +7,11 @@ const S = require('string');
 const { encrypt } = require('@nelreina/node-crypto');
 
 S.extendPrototype();
-const writeFile = promisify(fs.writeFile);
+// const writeFile = promisify(fs.writeFile);
 const readFile = promisify(fs.readFile);
 const fileExists = promisify(fs.exists);
 
-module.exports = async (dirname, envs) => {
-  const dotenvFile = path.resolve(dirname, './.env');
+module.exports = async (dotenvFile, envs) => {
   const existingEnvs = {};
   if (await fileExists(dotenvFile)) {
     const envArray = (await readFile(dotenvFile)).toString().split(EOL);
@@ -34,6 +33,37 @@ module.exports = async (dirname, envs) => {
       text += `${key}='${value}'${EOL}`;
     }
   });
-  await writeFile(dotenvFile, text);
-  return true;
+  return text;
 };
+
+/** TODO: NEED TO SUPPORT BOTH OPTIONS
+ * Write File
+ * Return Text
+ */
+
+// module.exports = async (dirname, envs) => {
+//   const dotenvFile = path.resolve(dirname, './.env');
+//   const existingEnvs = {};
+//   if (await fileExists(dotenvFile)) {
+//     const envArray = (await readFile(dotenvFile)).toString().split(EOL);
+//     envArray.forEach(line => {
+//       if (line.contains('=')) {
+//         const [key, value] = line.split('=');
+//         /*eslint-disable */
+//         existingEnvs[key] = value.replaceAll(`'`, '');
+//         /*eslint-enable */
+//       }
+//     });
+//   }
+//   let text = '';
+//   each(assign({}, existingEnvs, envs), (value, key) => {
+//     if (key.contains('PWD') || key.contains('PASSWORD')) {
+//       const hashedValue = encrypt(value);
+//       text += `${key}='${hashedValue}'${EOL}`;
+//     } else {
+//       text += `${key}='${value}'${EOL}`;
+//     }
+//   });
+//   await writeFile(dotenvFile, text);
+//   return true;
+// };
