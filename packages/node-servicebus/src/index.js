a5233282eee2861515a4d5249f16085dd8256d49
Nelson Reina (@nelreina) <nelreina@gmail.com>
2018-07-04 22:38:41 -0400

    node-servicebus: added

---
+++ packages/node-servicebus/src/index.js
@@ -0,0 +1,16 @@
+const ServiceBus = require('servicebus');
+const retry = require('servicebus-retry');
+const messageDomain = require('servicebus-message-domain');
+module.exports = (options = {}) => {
+  const bus = ServiceBus.bus({ url: options.url });
+  bus.use(
+    retry({
+      store: new retry.MemoryStore()
+    })
+  );
+  bus.use(messageDomain());
+  bus.use(bus.logger());
+  options.package && bus.use(bus.package());
+  options.correlate && bus.use(bus.correlate());
+  return bus;
+};
