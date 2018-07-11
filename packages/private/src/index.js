6665e05716435b0d42490645e06cd0707a02b981
Nelson Reina (@nelreina) <nelreina@gmail.com>
2018-07-11 06:21:00 -0400

    private package to test functionalities

---
+++ packages/private/src/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const log4js = require('@nelreina/node-log4js');
+const servicebus = require('@nelreina/node-servicebus');
+const { mssqlConn, invokeSQLCmd: Q } = require('@nelreina/node-sequelize');
+const CronJob = require('cron').CronJob;
+
+const logger = log4js('private');
+const mssql = mssqlConn(false, logger);
+const bus = servicebus({ package: true });
+
+let count = 0;
+logger.info('Log4js works');
+
+const sql = 'select * from model.Report';
+
+const sendCron = async () => {
+  const data = await Q(mssql, sql, true);
+  // logger.trace(data);
+  const payload = Object.assign({}, data, { counter: ++count });
+  bus.send('cron-1sec', payload);
+};
+
+bus.listen('cron-1sec', evt => logger.info(JSON.stringify(evt, null, 2)));
+new CronJob(
+  '*/10 * * * * *',
+  sendCron,
+  // () => ,
+  null,
+  true
+);
