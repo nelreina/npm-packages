e06fc413f305b88084295f199ed9b73ce9533a44
Nelson Reina (@nelreina) <nelreina@gmail.com>
2018-10-15 11:44:12 -0400

    web-redux: logger middelware only in development

--- packages/web-redux/src/createStore.js
+++ packages/web-redux/src/createStore.js
@@ -1,19 +1,29 @@
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
-import Api from "@nelreina/web-api";
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import Api from '@nelreina/web-api';
 const logger = store => next => action => {
   console.group(action.type);
-  console.info("dispaching", action);
+  console.info('dispaching', action);
   const result = next(action);
-  console.log("next state", store.getState());
+  console.log('next state', store.getState());
   console.groupEnd(action.type);
   return result;
 };
 
-export default (rootReducer, initialState = {}, host = "") => {
+export default (
+  rootReducer,
+  initialState = {},
+  host = '',
+  production = false
+) => {
   const api = new Api(host);
-  const middelwares = applyMiddleware(thunk.withExtraArgument(api), logger);
+  let middelwares;
+  if (production) {
+    middelwares = applyMiddleware(thunk.withExtraArgument(api));
+  } else {
+    middelwares = applyMiddleware(thunk.withExtraArgument(api), logger);
+  }
 
   const enhancer = composeWithDevTools(middelwares);
 
