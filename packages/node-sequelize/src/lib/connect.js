5ac1a0e537ba70d630577e278c25c439ccc73d47
Nelson Reina (@nelreina) <nelreina@gmail.com>
2018-11-17 12:18:43 -0400

    node-sequelize: made connect function a promise

--- packages/node-sequelize/src/lib/connect.js
+++ packages/node-sequelize/src/lib/connect.js
@@ -5,19 +5,26 @@ const DB_HOST = process.env.DB_HOST;
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
-module.exports = (dialect, DB_NAME = process.env.DB_NAME, logger = console) => {
-  if (!dialect) {
-    throw new Error('Please provide a dialect (mysql|sqlite|postgres) !');
-  }
-  if (dialect === 'mssql') {
-    throw new Error('Please use "mssql-conn" object in this library!');
-  }
+module.exports = (dialect, DB_NAME = process.env.DB_NAME, logger = console) =>
+  new Promise(async (resolve, reject) => {
+    if (!dialect) {
+      reject('Please provide a dialect (mysql|sqlite|postgres) !');
+    }
+    if (dialect === 'mssql') {
+      reject('Please use "mssql-conn" object in this library!');
+    }
 
-  const authConn = `Server=${DB_HOST};Database=${DB_NAME};Username:${DB_USERNAME};`;
+    try {
+      const authConn = `Server=${DB_HOST};Database=${DB_NAME};Username:${DB_USERNAME};`;
 
-  logger.info(`${dialect} connection: ${authConn}`);
-  const options = { dialect };
-  options['operatorsAliases'] = false;
-  options['logging'] = false;
-  return new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, options);
-};
+      logger.info(`${dialect} connection: ${authConn}`);
+      const options = { dialect };
+      options['operatorsAliases'] = false;
+      options['logging'] = false;
+      const db = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, options);
+      await db.authenticate();
+      resolve(db);
+    } catch (error) {
+      reject(error);
+    }
+  });
