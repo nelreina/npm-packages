48d71fef6147e414989643e674c1c1955c3c22d8
Nelson Reina (@nelreina) <nelreina@gmail.com>
2018-11-17 21:15:19 -0400

    node-sequelize: export connect function

--- packages/node-sequelize/src/index.js
+++ packages/node-sequelize/src/index.js
@@ -3,4 +3,5 @@ exports.getConnection = require('./lib/get-connection');
 exports.invokeSQLCmd = require('./lib/invoke-sql-cmd');
 exports.bulkInsert = require('./lib/bulk-insert');
 exports.mssqlConn = require('./lib/mssql-conn');
+exports.connect = require('./lib/connect');
 exports.findAndUpdateModel = require('./lib/findAndUpdateModel');
