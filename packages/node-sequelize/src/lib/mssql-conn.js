a4a37ae62e1a6694a65d0a4c80bae16bee03f63a
Nelson Reina (@nelreina) <nelreina@gmail.com>
2018-11-18 02:27:44 -0400

    node-sequelize: promisfy  mssql-conn

--- packages/node-sequelize/src/lib/mssql-conn.js
+++ packages/node-sequelize/src/lib/mssql-conn.js
@@ -7,38 +7,46 @@ const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_PORT = process.env.DB_PORT;
 const DB_INTEGRATED = process.env.DB_INTEGRATED;
 
-module.exports = (dbName, logger = console) => {
-  if (!DB_INTEGRATED) {
-    logger.error('env variable DB_INTEGRATED is required!');
-    throw new Error('ERR: env variable DB_INTEGRATED is required!');
-  }
-  const DB_NAME = dbName || process.env.DB_NAME;
+module.exports = (DB_NAME = process.env.DB_NAME, logger = console) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      if (!DB_INTEGRATED) {
+        reject({ message: 'ERR: env variable DB_INTEGRATED is required!' });
+      }
 
-  const trustedConn = `Server=${DB_HOST};Database=${DB_NAME};Trusted_Connection=yes;`;
-  const authConn = `Server=${DB_HOST};Database=${DB_NAME};Username:${DB_USERNAME};`;
+      const trustedConn = `Server=${DB_HOST};Database=${DB_NAME};Trusted_Connection=yes;`;
+      const authConn = `Server=${DB_HOST};Database=${DB_NAME};Username:${DB_USERNAME};`;
 
-  const integrated = DB_INTEGRATED === 'Y';
-  logger.info('mssql connection', integrated ? trustedConn : authConn);
-  const conn = {};
-  conn['dialect'] = 'mssql';
-  conn['operatorsAliases'] = false;
-  conn['logging'] = false;
-  if (!integrated) {
-    conn['username'] = DB_USERNAME;
-    conn['password'] = DB_PASSWORD;
-    conn['database'] = DB_NAME;
-    conn['host'] = DB_HOST;
-    conn['dialectOptions'] = {
-      encrypt: true,
-      requestTimeout: 0,
-      connectionTimeout: 9999999999
-    };
-    if (DB_PORT) {
-      conn['port'] = DB_PORT;
+      const integrated = DB_INTEGRATED === 'Y';
+      const conn = {};
+      conn['dialect'] = 'mssql';
+      conn['operatorsAliases'] = false;
+      conn['logging'] = false;
+      if (!integrated) {
+        conn['username'] = DB_USERNAME;
+        conn['password'] = DB_PASSWORD;
+        conn['database'] = DB_NAME;
+        conn['host'] = DB_HOST;
+        conn['dialectOptions'] = {
+          encrypt: true,
+          requestTimeout: 0,
+          connectionTimeout: 9999999999
+        };
+        if (DB_PORT) {
+          conn['port'] = DB_PORT;
+        }
+      } else {
+        conn['dialectModulePath'] = 'sequelize-msnodesqlv8';
+        conn['dialectOptions'] = { connectionString: trustedConn };
+      }
+
+      const sequelize = new Sequelize(conn);
+      await sequelize.authenticate();
+      logger.info(
+        `mssql connect success ${integrated ? trustedConn : authConn}`
+      );
+      resolve(sequelize);
+    } catch (error) {
+      reject(error);
     }
-  } else {
-    conn['dialectModulePath'] = 'sequelize-msnodesqlv8';
-    conn['dialectOptions'] = { connectionString: trustedConn };
-  }
-  return new Sequelize(conn);
-};
+  });
