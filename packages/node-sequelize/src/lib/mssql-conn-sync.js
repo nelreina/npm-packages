07820690f6feb96390cbe17233a3a87d9a6c2a32
Nelson Reina (@nelreina) <nelreina@gmail.com>
2018-12-12 04:01:46 -0400

    node sequelize: added connection pool options

--- packages/node-sequelize/src/lib/mssql-conn-sync.js
+++ packages/node-sequelize/src/lib/mssql-conn-sync.js
@@ -6,7 +6,12 @@ const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_PORT = process.env.DB_PORT;
 const DB_INTEGRATED = process.env.DB_INTEGRATED;
-
+const pool = {
+  max: process.env.DB_POOL_MAX || 10,
+  min: process.env.DB_POOL_MIN || 0,
+  idle: process.env.DB_POOL_IDLE || 20000,
+  acquire: process.env.DB_POOL_ACQUIRE || 20000
+};
 module.exports = (dbName, logger = console) => {
   if (!DB_INTEGRATED) {
     logger.error('env variable DB_INTEGRATED is required!');
@@ -28,6 +33,7 @@ module.exports = (dbName, logger = console) => {
     conn['password'] = DB_PASSWORD;
     conn['database'] = DB_NAME;
     conn['host'] = DB_HOST;
+    conn['options'] = { pool };
     conn['dialectOptions'] = {
       encrypt: true,
       requestTimeout: 0,
