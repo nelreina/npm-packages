474b3a1b2867a580a9c308c4c9c75916de10422c
Nelson Reina (@nelreina) <nelreina@gmail.com>
2019-10-23 12:37:09 -0400

    node-squelize: upgrade packages to latest version and removed db_integarted

--- packages/node-sequelize/src/lib/mssql-conn-sync.js
+++ packages/node-sequelize/src/lib/mssql-conn-sync.js
@@ -5,7 +5,6 @@ const DB_HOST = process.env.DB_HOST;
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_PORT = process.env.DB_PORT;
-const DB_INTEGRATED = process.env.DB_INTEGRATED;
 const pool = {
   max: parseInt(process.env.DB_POOL_MAX, 0) || 10,
   min: parseInt(process.env.DB_POOL_MIN, 0) || 0,
@@ -13,46 +12,35 @@ const pool = {
   acquire: parseInt(process.env.DB_POOL_ACQUIRE, 0) || 20000
 };
 module.exports = (dbName, logger = console) => {
-  if (!DB_INTEGRATED) {
-    logger.error('env variable DB_INTEGRATED is required!');
-    throw new Error('ERR: env variable DB_INTEGRATED is required!');
-  }
   const DB_NAME = dbName || process.env.DB_NAME;
 
-  const trustedConn = `Server=${DB_HOST};Database=${DB_NAME};Trusted_Connection=yes;`;
   const authConn = `Server=${DB_HOST};Database=${DB_NAME};Username:${DB_USERNAME};`;
 
-  const integrated = DB_INTEGRATED === 'Y';
-  logger.info('mssql connection', integrated ? trustedConn : authConn);
+  logger.info('mssql connection', authConn);
   const conn = {};
   conn['dialect'] = 'mssql';
   conn['operatorsAliases'] = false;
   conn['logging'] = false;
-  if (!integrated) {
-    const dialectOptions = {
-      encrypt: true,
-      requestTimeout: parseInt(process.env.DB_REQUEST_TIMEOUT, 0) || 0,
-      connectTimeout: parseInt(process.env.DB_CONNECT_TIMEOUT, 0) || 0
-    };
 
-    const options = {
-      host: DB_HOST,
-      dialect: 'mssql',
-      operatorsAliases: false,
-      pool,
-      dialectOptions
-    };
-    if (!process.env.DB_LOGGING) {
-      options['logging'] = false;
-    }
+  const dialectOptions = {
+    encrypt: true,
+    requestTimeout: parseInt(process.env.DB_REQUEST_TIMEOUT, 0) || 0,
+    connectTimeout: parseInt(process.env.DB_CONNECT_TIMEOUT, 0) || 0
+  };
 
-    if (DB_PORT) {
-      conn['port'] = DB_PORT;
-    }
-    return new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, options);
-  } else {
-    conn['dialectModulePath'] = 'sequelize-msnodesqlv8';
-    conn['dialectOptions'] = { connectionString: trustedConn };
-    return new Sequelize(conn);
+  const options = {
+    host: DB_HOST,
+    dialect: 'mssql',
+    operatorsAliases: false,
+    pool,
+    dialectOptions
+  };
+  if (!process.env.DB_LOGGING) {
+    options['logging'] = false;
   }
+
+  if (DB_PORT) {
+    conn['port'] = DB_PORT;
+  }
+  return new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, options);
 };
