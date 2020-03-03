6d410645789aa8ba7a4d6224934dea7a3427d3f5
Nelson Reina (@nelreina) <nelreina@gmail.com>
2020-03-03 14:35:57 -0400

    react-component: changed export in index

--- packages/react-components/src/index.js
+++ packages/react-components/src/index.js
@@ -1,9 +1,19 @@
-export UiDynamicTable from './components/DynamicTable';
+import UiDynamicTable from './components/DynamicTable';
+import * as StyledBootstrap from './components/styled-bootstrap';
+import Button from './components/Button';
+import Icon from './components/Icon';
+import Moment from './components/Moment';
+import Popover from './components/Popover';
+import Spinner from './components/Spinner';
+import Modal from './components/BootstrapModal';
 
-export Button from './components/Button';
-export StyledBootstrap from './components/styled-bootstrap';
-export Icon from './components/Icon';
-export Moment from './components/Moment';
-export Popover from './components/Popover';
-export Spinner from './components/Spinner';
-export Modal from './components/BootstrapModal';
+export {
+  StyledBootstrap,
+  UiDynamicTable,
+  Button,
+  Icon,
+  Moment,
+  Popover,
+  Spinner,
+  Modal
+};
